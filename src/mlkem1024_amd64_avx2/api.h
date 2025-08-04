6223640956cf3ed8a85ee0119368e12693a977ed
Manuel Barbosa <mbbarbosa@gmail.com>
2025-08-04 15:59:03 +0000

    started 1024 branch
    Signed-off-by: Tiago Oliveira <tfaoliveira@gmail.com>

---
+++ src/mlkem1024_amd64_avx2/api.h
@@ -0,0 +1,37 @@
+#ifndef jade_kem_mlkem_mlkem768_amd64_avx2_API_H
+#define jade_kem_mlkem_mlkem768_amd64_avx2_API_H
+
+#include <stdint.h>
+
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_SECRETKEYBYTES 3168
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_PUBLICKEYBYTES 1568
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_CIPHERTEXTBYTES 1568
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_KEYPAIRCOINBYTES 64
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_ENCCOINBYTES 32
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_BYTES 32
+
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_ALGNAME "mlkem1024"
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_ARCH "amd64"
+#define jade_kem_mlkem_mlkem1024_amd64_avx2_IMPL "avx2"
+
+int jade_kem_mlkem_mlkem1024_amd64_avx2_keypair_derand(
+  uint8_t *public_key,
+  uint8_t *secret_key,
+  const uint8_t *coins
+);
+
+int jade_kem_mlkem_mlkem1024_amd64_avx2_enc_derand(
+  uint8_t *ciphertext,
+  uint8_t *shared_secret,
+  const uint8_t *public_key,
+  const uint8_t *coins
+);
+
+int jade_kem_mlkem_mlkem1024_amd64_avx2_dec(
+  uint8_t *shared_secret,
+  const uint8_t *ciphertext,
+  const uint8_t *secret_key
+);
+
+
+#endif
